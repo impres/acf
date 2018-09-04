58700b61426fb7aa3d18910db76f74a5cff01c1f
Kevin zwarts <kzwarts@impres.nl>
2018-09-04 15:57:09 +0200

    Fix for logics that contain any depth if the guid is not correctly available. Fetching the permalinks solves this problem. Alsof fixed the problem that archive links were not fetched. Archive links do not contain any post so the script would return an exception.

--- src/Field/PageLink.php
+++ src/Field/PageLink.php
@@ -12,17 +12,34 @@ use Corcel\Acf\FieldInterface;
 class PageLink extends PostObject implements FieldInterface
 {
     /**
+     * Extend the process function to be able to fetch the archive link that can be set with PageLink
+     *
+     * @param string $fieldName
+     */
+    public function process($fieldName)
+    {
+        // Let parent process the field name
+        parent::process($fieldName);
+
+        // If object is set with an object overwrite it with the value from the database (string URL)
+        if (!$this->object) {
+            $this->object = $this->fetchValue($fieldName);
+        }
+    }
+
+    /**
      * @return string
      */
     public function get()
     {
         $page = parent::get();
-        $domain = substr($page->guid, 0, strpos($page->guid, '?'));
 
-        if (empty($page->post_name)) {
-            return $page->guid;
+        // Check if page is an object, if it's not an object it is most likely an archive link and page is a string containing the url
+        if (!is_object($page) && is_string($page)) {
+            return $page ?? '';
         }
 
-        return "{$domain}{$page->post_name}/";
+        // Get the page permalink (also works with depth in page url's and WPML)
+        return get_the_permalink($page->ID);
     }
 }
